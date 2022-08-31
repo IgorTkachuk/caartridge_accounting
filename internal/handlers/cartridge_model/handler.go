3ebd8269aec7ad74380d2e60c01f184234fd21ef
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-08-31 10:47:52 +0300

    feat: implement rest CRUD API endpoints for cartridge model

--- internal/handlers/cartridge_model/handler.go
+++ internal/handlers/cartridge_model/handler.go
@@ -2,6 +2,7 @@ package cartridge_model
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/IgorTkachuk/cartridge_accounting/internal/apperror"
 	"github.com/IgorTkachuk/cartridge_accounting/internal/domain/cartridge_model"
 	"github.com/IgorTkachuk/cartridge_accounting/internal/handlers"
@@ -9,6 +10,7 @@ import (
 	"github.com/IgorTkachuk/cartridge_accounting/pkg/logging"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -24,6 +26,10 @@ type Handler struct {
 
 func (h Handler) Register(r *httprouter.Router) {
 	r.HandlerFunc(http.MethodGet, cartridgeModelsUrl, jwt.Middleware(apperror.Middleware(h.GetCarteidgeModels)))
+	r.HandlerFunc(http.MethodGet, cartridgeModelUrl, jwt.Middleware(apperror.Middleware(h.GetCartridgeModelById)))
+	r.HandlerFunc(http.MethodPost, cartridgeModelsUrl, jwt.Middleware(apperror.Middleware(h.CreateCartridgeModel)))
+	r.HandlerFunc(http.MethodPatch, cartridgeModelsUrl, jwt.Middleware(apperror.Middleware(h.UpdateCartridgeModel)))
+	r.HandlerFunc(http.MethodDelete, cartridgeModelUrl, jwt.Middleware(apperror.Middleware(h.DeleteCartridgeModel)))
 }
 
 func (h Handler) GetCarteidgeModels(w http.ResponseWriter, r *http.Request) error {
@@ -42,5 +48,76 @@ func (h Handler) GetCarteidgeModels(w http.ResponseWriter, r *http.Request) erro
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(modelsBytes)
+	return nil
+}
+
+func (h Handler) GetCartridgeModelById(w http.ResponseWriter, r *http.Request) error {
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	modelId, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		return err
+	}
+
+	ctrModel, err := h.CartridgeModelSvc.GetById(r.Context(), modelId)
+	if err != nil {
+		return err
+	}
+
+	ctrModelBytes, err := json.Marshal(ctrModel)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(ctrModelBytes)
+
+	return nil
+}
+
+func (h Handler) CreateCartridgeModel(w http.ResponseWriter, r *http.Request) error {
+	defer r.Body.Close()
+
+	var newCtrModel cartridge_model.CreateCartridgeModelDTO
+	err := json.NewDecoder(r.Body).Decode(&newCtrModel)
+	if err != nil {
+		return apperror.BadRequestError("Failed to decode new cartridge model data")
+	}
+
+	newCtrModelId, err := h.CartridgeModelSvc.Create(r.Context(), newCtrModel)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s/%s", cartridgeModelsUrl, newCtrModelId))
+	return nil
+}
+
+func (h Handler) UpdateCartridgeModel(w http.ResponseWriter, r *http.Request) error {
+	defer r.Body.Close()
+
+	var updCtrModelInfo cartridge_model.UpdateCartridgeModelDTO
+	if err := json.NewDecoder(r.Body).Decode(&updCtrModelInfo); err != nil {
+		return apperror.BadRequestError("Failed to decode info for update cartridge model")
+	}
+
+	err := h.CartridgeModelSvc.Update(r.Context(), updCtrModelInfo)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	return nil
+}
+
+func (h Handler) DeleteCartridgeModel(w http.ResponseWriter, r *http.Request) error {
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	ctrModelId, _ := strconv.Atoi(params.ByName("id"))
+
+	err := h.CartridgeModelSvc.Delete(r.Context(), ctrModelId)
+	if err != nil {
+		return err
+	}
+	w.WriteHeader(http.StatusNoContent)
 	return nil
 }
