913f4b79b23c4609d0bc044cd97ca7bf8eff6afe
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-09-06 17:49:35 +0300

    fix: OU model

--- internal/domain/ou/model.go
+++ internal/domain/ou/model.go
@@ -4,18 +4,18 @@ type Ou struct {
 	ID             int    `json:"id,omitempty"`
 	Name           string `json:"name,omitempty"`
 	ParentId       *int   `json:"parent_id"`
-	BusinessLineId *int   `json:"businessLineId"`
+	BusinessLineId *int   `json:"business_line_id"`
 }
 
 type UpdateOuDTO struct {
 	ID             int    `json:"id,omitempty"`
 	Name           string `json:"name,omitempty"`
 	ParentId       *int   `json:"parent_id"`
-	BusinessLineId *int   `json:"businessLineId"`
+	BusinessLineId *int   `json:"business_line_id"`
 }
 
 type CreateOuDTO struct {
 	Name           string `json:"name,omitempty"`
 	ParentId       *int   `json:"parent_id,omitempty"`
-	BusinessLineId *int   `json:"businessLineId"`
+	BusinessLineId *int   `json:"business_line_id"`
 }
