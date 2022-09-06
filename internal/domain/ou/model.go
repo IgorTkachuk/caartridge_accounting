446666d6386e61888352dd327dad2da9ac115c51
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-09-06 09:58:07 +0300

    fix: add Readme.md with migrate utility and Makefile description, some litle change in ou model

--- internal/domain/ou/model.go
+++ internal/domain/ou/model.go
@@ -3,8 +3,8 @@ package ou
 type Ou struct {
 	ID             int    `json:"id,omitempty"`
 	Name           string `json:"name,omitempty"`
-	ParentId       int    `json:"parent_id,omitempty"`
-	BusinessLineId int    `json:"businessLineId"`
+	ParentId       *int   `json:"parent_id"`
+	BusinessLineId *int   `json:"businessLineId"`
 }
 
 type UpdateOuDTO struct {
