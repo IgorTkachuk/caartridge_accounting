210538773e25f62ed3b8064cf2d7f2d086025eb5
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-09-21 15:33:02 +0300

    feat: add status fiels to docType entity

--- internal/domain/doc_type/model.go
+++ internal/domain/doc_type/model.go
@@ -1,18 +1,21 @@
 package doc_type
 
 type DocType struct {
-	ID              int    `json:"id,omitempty"`
-	Name            string `json:"name,omitempty"`
-	CtrStatusTypeId int    `json:"ctr_status_type_id"`
+	ID                int    `json:"id,omitempty"`
+	Name              string `json:"name,omitempty"`
+	CtrStatusTypeFrom int    `json:"ctr_status_type_from"`
+	CtrStatusTypeTo   int    `json:"ctr_status_type_to"`
 }
 
 type CreateDocTypeDTO struct {
-	Name            string `json:"name,omitempty"`
-	CtrStatusTypeId int    `json:"ctr_status_type_id"`
+	Name              string `json:"name,omitempty"`
+	CtrStatusTypeFrom int    `json:"ctr_status_type_from"`
+	CtrStatusTypeTo   int    `json:"ctr_status_type_to"`
 }
 
 type UpdateDocTypeDTO struct {
-	ID              int    `json:"id,omitempty"`
-	Name            string `json:"name,omitempty"`
-	CtrStatusTypeId int    `json:"ctr_status_type_id"`
+	ID                int    `json:"id,omitempty"`
+	Name              string `json:"name,omitempty"`
+	CtrStatusTypeFrom int    `json:"ctr_status_type_from"`
+	CtrStatusTypeTo   int    `json:"ctr_status_type_to"`
 }
